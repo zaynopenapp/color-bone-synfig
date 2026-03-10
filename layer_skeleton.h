2e72e33554b058d704d4ddd73b3c4228b9649879
Al Zayn <robert.mailcat@gmail.com>
2026-03-11 06:18:52 +0700

    Add files via upload

---
+++ layer_skeleton.h
@@ -0,0 +1,81 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file layer_skeleton.h
+**	\brief Header file for implementation of the "Layer_Skeleton" layer
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**
+**	This file is part of Synfig.
+**
+**	Synfig is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 2 of the License, or
+**	(at your option) any later version.
+**
+**	Synfig is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with Synfig.  If not, see <https://www.gnu.org/licenses/>.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_LAYER_SKELETON_H
+#define __SYNFIG_LAYER_SKELETON_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <synfig/bone.h>
+#include <synfig/color.h>
+#include "layer_polygon.h"
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+namespace synfig {
+
+class Layer_Skeleton : public Layer_Polygon
+{
+	SYNFIG_LAYER_MODULE_EXT
+private:
+
+	//!Parameter: (std::vector<synfig::Bone>) Bones list of the skeleton
+	ValueBase param_bones;
+	//!Parameter: (synfig::String) Name of the skeleton
+	ValueBase param_name;
+
+	ValueBase color_bones;
+
+public:
+	typedef etl::handle<Layer_Skeleton> Handle;
+	typedef etl::handle<const Layer_Skeleton> ConstHandle;
+	typedef etl::loose_handle<Layer_Skeleton> LooseHandle;
+
+	Layer_Skeleton();
+
+#ifdef _DEBUG
+	~Layer_Skeleton();
+#endif
+
+	virtual bool set_param(const synfig::String & param, const synfig::ValueBase &value);
+	virtual synfig::ValueBase get_param(const synfig::String & param)const;
+
+	virtual Vocab get_param_vocab()const;
+
+protected:
+	virtual void sync_vfunc();
+}; // END of class Layer_Skeleton
+
+}; // END of namespace synfig
+
+/* === E N D =============================================================== */
+
+#endif
